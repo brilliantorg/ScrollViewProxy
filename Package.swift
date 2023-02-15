f0f97d5edf5ae2809af836aa84a456b61e3a6bba
Evan Brass <evanbrass@gmail.com>
2023-02-15 16:53:57 -0500

    Try introspect v 0.2.2

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
 //        .package(name: "Introspect", url: "https://github.com/siteline/SwiftUI-Introspect.git", from: "0.2.1")
-        .package(name: "Introspect", url: "https://github.com/siteline/SwiftUI-Introspect.git", "0.1.4"..<"0.2.0")
+        .package(name: "Introspect", url: "https://github.com/siteline/SwiftUI-Introspect.git", "0.2.2"..."0.2.2")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
